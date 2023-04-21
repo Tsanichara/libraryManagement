3d132716c59cf01ec48c7a3fd54bc0ae42bf41fc
Tyler Sanichara <Tsanichara67@gmail.com>
2023-04-21 12:50:10 -0400

    Defined getters, setters and constructors using lombok

--- src/main/java/com/application/libraryManagement/entity/Author.java
+++ src/main/java/com/application/libraryManagement/entity/Author.java
@@ -1,10 +1,16 @@
 package com.application.libraryManagement.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "authors")
 public class Author {
